f8ad51a3ed6540a7099741d7be7c60c588a1f217
mazax1ct <vova@giveandget.ru>
2018-12-05 20:41:48 +0300

    new config

--- src/js/config.js
+++ src/js/config.js
@@ -60,12 +60,14 @@ $(document).ready(function() {
       $(".config").removeClass("blur");
       $(".main-menu").removeClass("is-open");
       $(".footer").removeClass("blur");
+      $(".nc").removeClass("blur");
     } else {
       $(this).addClass("is-active");
       $("body").addClass("overflow");
       $(".config").addClass("blur");
       $(".main-menu").addClass("is-open");
       $(".footer").addClass("blur");
+      $(".nc").addClass("blur");
     }
     $(".config__content").removeClass("blur");
     $(".config__intro").removeClass("blur");
@@ -88,12 +90,14 @@ $(document).ready(function() {
       $(".config").removeClass("blur");
       $(".personal-menu").removeClass("is-open");
       $(".footer").removeClass("blur");
+      $(".nc").removeClass("blur");
     } else {
       $(this).addClass("is-active");
       $("body").addClass("overflow");
       $(".config").addClass("blur");
       $(".personal-menu").addClass("is-open");
       $(".footer").addClass("blur");
+      $(".nc").addClass("blur");
     }
     $(".config__content").removeClass("blur");
     $(".config__intro").removeClass("blur");
@@ -324,5 +328,91 @@ $(document).ready(function() {
   //открытие нового блока производительности
   $(".js-performance").click(function () {
     $(".nc__performance-dropdown").toggleClass("is-open");
-  })
+  });
+
+  //открытие скрытого блока с показателями производительности
+  $(".js-hidden-bars-opener").click(function () {
+    $(".nc__performance-bars-hidden").toggleClass("is-open");
+    if($(".nc__performance-bars-hidden").hasClass("is-open")){
+      $(this).text($(this).attr("data-text2"));
+    } else {
+      $(this).text($(this).attr("data-text1"));
+    }
+  });
+
+  //переключение выводимых параметров производительности
+  $(".js-performance-type .nc__performance-tabs-link").click(function () {
+    $(".js-performance-type .nc__performance-tabs-link").removeClass("is-active");
+    $(this).addClass("is-active");
+    $(".nc__performance-bars-tab").removeClass("is-active");
+    $(".nc__performance-bars-tab[data-target=" + $(this).attr("data-target") + "]").addClass("is-active");
+    return false;
+  });
+
+  //открытие списка компонентов на 992
+  $(".js-nc-c-block").click(function () {
+    $(".nc-c").removeClass("is-open");
+    $(".js-nc-c-block").removeClass("is-open");
+    $(this).addClass("is-open");
+    $(".nc-c__list").removeClass("is-open");
+    $(this).parent(".nc-c").addClass("is-open");
+    $(this).next(".nc-c__list").addClass("is-open");
+    if($(this).hasClass("is-open")){
+      $(".nc").addClass("is-open");
+    } else {
+      $(".nc").removeClass("is-open");
+    }
+  });
+
+  //закрытие списка компонентов на 992
+  $(".js-nc-c-block-close").click(function () {
+    $(".js-nc-c-block").removeClass("is-open");
+    $(".nc-c__list").removeClass("is-open");
+    $(".nc").removeClass("is-open");
+  });
+
+  //листалка "слайдов" конфига
+  $(".nc-c-list__arrow").click(function () {
+    var slides = $('.nc-c-list__item').length; //кол-во слайдов
+
+    if(slides > 1) { //если есть другие слайды
+
+      if($(this).hasClass("nc-c-list__arrow--next")) { //если движемся вперед
+
+        var slide = $(".nc-c-list__item.is-active").index();
+        console.log("вперед", slide, slides);
+
+        if ((slide + 1) < slides) {
+          $(".nc-c-list__item").removeClass("is-active");
+          $(".nc-c-list__item").eq(slide + 1).addClass("is-active");
+          $(".nc-c-list__arrow--prev").prop("disabled", false);
+        }
+
+        if (slide + 2 >= slides) {
+          $(".nc-c-list__arrow--next").prop("disabled", true);
+          return false;
+        }
+      }
+
+      if($(this).hasClass("nc-c-list__arrow--prev")) { //если движемся назад
+        var slide = $(".nc-c-list__item.is-active").index();
+
+        console.log("назад");
+        if(slide > 0) {
+          $(".nc-c-list__item").removeClass("is-active");
+          $(".nc-c-list__item").eq(slide - 1).addClass("is-active");
+          $(".nc-c-list__arrow--next").prop("disabled", false);
+        }
+
+        if(slide - 1 == 0) {
+          $(".nc-c-list__arrow--prev").prop("disabled", true);
+          return false;
+        }
+      }
+
+    } else { //если слайдов нет, то кнопки отключаем на php при формировании шаблона
+      console.log("слайдов 1, нужно отключить кнопки");
+    }
+
+  });
 });
